36aa3970164f7d273902d1f83680b4a4ef22b112
Andrew Tischenko <31860133+antis0007@users.noreply.github.com>
2026-02-12 17:20:19 -0700

    updated, switching from laptop to PC

--- ListyCity/app/src/main/java/com/example/listycity/CityList.java
+++ ListyCity/app/src/main/java/com/example/listycity/CityList.java
@@ -30,4 +30,38 @@ public class CityList {
         Collections.sort(list);
         return list;
     }
+    public boolean hasCity(City city){
+        //When given a city, return whether or not it belongs in the list
+        //TODO: Test to see whether your method is correct
+        //TODO: (Read https://www.infoworld.com/article/3305792/comparing-java-objects-with-equals-and-hashcode.html)
+        for (int i = 0; i<cities.size(); i++){
+            if(city.equals(cities.get(i))){return true;};
+        }
+        return false;
+    }
+    public void delete(City city){
+
+        //Check if a city is present in the list. If it does then remove it from the list, if not then **throw an exception**
+        //TODO: Test to see if your method actually removes it from the list
+        //TODO: Test to see if the exception is actually thrown (Read more here https://howtodoinjava.com/junit5/expected-exception-example)
+        if (hasCity(city)) {
+            String cityName = city.getCityName();
+            if (cityName == null || cityName.isEmpty()){
+                throw new IllegalArgumentException("City name not defined!");
+            }
+            else {
+                cities.remove(cityName);
+            }
+        }
+        else {
+            throw new IllegalArgumentException("City not found in the list!");
+        }
+
+    }
+    public int countCities(){
+        //Return how many cities are in the list:
+        //TODO:Test to see whether your method is correct
+        int count = cities.size();
+        return count;
+    }
 }
--- ListyCity/app/src/test/java/com/example/listycity/CityListTest.java
+++ ListyCity/app/src/test/java/com/example/listycity/CityListTest.java
@@ -1,6 +1,8 @@
 package com.example.listycity;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CityListTest {
     private CityList mockCityList() {
         CityList cityList = new CityList();
@@ -40,5 +42,35 @@ class CityListTest {
 // Now the original city should be at position 1
         assertEquals(0, city.compareTo(cityList.getCities().get(0)));
         assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
+    }
+    @Test
+    void testHasCity(){
+        //When given a city, return whether or not it belongs in the list
+        //TODO: Test to see whether your method is correct
+        //TODO: (Read https://www.infoworld.com/article/3305792/comparing-java-objects-with-equals-and-hashcode.html)
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+        City cityOne = new City("Regina", "Saskatchewan");
+        cityList.add(cityOne);
+        City cityTwo = new City("Regina", "Saskatchewan");
+        boolean cityCheck = cityList.hasCity(cityTwo);
+        assertTrue(cityCheck);
+    }
+    @Test
+    void testDelete(){
+        //Check if a city is present in the list. If it does then remove it from the list, if not then **throw an exception**
+        //TODO: Test to see if your method actually removes it from the list
+        CityList cityList = mockCityList();
+        City cityOne = new City("Regina", "Saskatchewan");
+        cityList.add(cityOne);
+        cityList.delete(cityOne);
+        assertFalse(cityList.getCities().contains(cityOne), "List should not contain removed city");
+        //TODO: Test to see if the exception is actually thrown (Read more here https://howtodoinjava.com/junit5/expected-exception-example)
+        
+    }
+    @Test
+    void testCountCities(){
+        //Return how many cities are in the list:
+        //TODO:Test to see whether your method is correct
     }
 }
